90c5173ffb18977e5e6e500a1797669e8b5fd40d
Oksana <ksenia79460@gmail.com>
2022-03-11 15:38:13 +0000

    set countdown timer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Tabs
 const tabParent = document.querySelector(".tabheader__items");
 const tabs = document.querySelectorAll(".tabheader__item");
 const tabsContent = document.querySelectorAll(".tabcontent");
@@ -34,3 +35,60 @@ tabParent.addEventListener("click", (event) => {
     });
   }
 });
+
+//Timer
+const deadline = "2022-03-30";
+
+//endtime - is deadline in milliseconds;
+function timeRemaining(endtime) {
+  //Date.parse() - converts to ms;
+  let t = Date.parse(endtime) - Date.parse(new Date());
+  //now ms convert in day, hours, min, sec
+  let days = Math.floor(t / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((t / (100 * 60 * 60)) % 24);
+  let min = Math.floor((t / (1000 * 60)) % 60);
+  let sec = Math.floor((t / 1000) % 60);
+
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    min: min,
+    sec: sec,
+  };
+}
+
+function zero(num) {
+  if (num >= 0 && num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+
+function setClock(selector, endtime) {
+  let timer = document.querySelector(selector);
+  let days = timer.querySelector("#days");
+  let hours = timer.querySelector("#hours");
+  let minutes = timer.querySelector("#minutes");
+  let seconds = timer.querySelector("#seconds");
+
+  timeInterval = setInterval(updateClock, 1000);
+
+  updateClock();
+
+  function updateClock() {
+    const t = timeRemaining(endtime);
+
+    days.innerHTML = zero(t.days);
+    hours.innerHTML = zero(t.hours);
+    minutes.innerHTML = zero(t.min);
+    seconds.innerHTML = zero(t.sec);
+
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
+}
+
+setClock(".timer", deadline);
